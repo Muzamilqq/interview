3da938d0f41e2b3053859b132536b4ebc1d24dc5
Muzamil <ashrafmuzamil329@gmail.com>
2026-03-01 16:11:48 +0500

    8 commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://distinguished-caring-production-6c96.up.railway.app",
+  baseURL: "https://distinguished-caring-production-6c96.up.railway.app/api",
   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
 });
 
