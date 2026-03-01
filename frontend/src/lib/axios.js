465abed6b8fb570252baea521aa4043224ed031d
Muzamil <ashrafmuzamil329@gmail.com>
2026-03-01 16:34:35 +0500

    hardcode api url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://distinguished-caring-production-6c96.up.railway.app/api",
-  withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
+  baseURL: "https://distinguished-caring-production-6c96.up.railway.app/api",
+  withCredentials: true,
 });
 
 export default axiosInstance;
