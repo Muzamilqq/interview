1b3bd5fe0feca243a928745f51fed5a7ccc5af85
Muzamil <ashrafmuzamil329@gmail.com>
2026-03-01 16:19:00 +0500

    fix api baseURL fallback

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://distinguished-caring-production-6c96.up.railway.app/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://distinguished-caring-production-6c96.up.railway.app/api",
   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
 });
 
